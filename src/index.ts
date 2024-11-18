838dd074e658e82421bcd1652696e6281a58db74
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2024-11-18 16:11:31 +0300

    implemented stops route

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { crawl, lastCrawl } from './crawler'
-import trips from './trips'
 import { shouldCrawl } from './helpers'
 import { cors } from 'hono/cors'
+import trips from './trips'
+import stops from './stops'
 
 const app = new Hono()
 app.use(cors())
 
 app.get('/', (c) => c.json(lastCrawl))
 app.route('/trips', trips)
+app.route('/stops', stops)
 
 setInterval(() => {
   if (shouldCrawl()) crawl()
