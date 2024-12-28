3705801e4b6d71b9c4e89e614b43b022e7ba1561
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2024-12-29 00:39:50 +0300

    OSRM env var

--- src/util/osrm.ts
+++ src/util/osrm.ts
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-const osrmHost = process.env.OSRM || 'http://osrm:5000'
-
 interface LatLng {
   lat: number
   lng: number
@@ -9,7 +7,7 @@ interface LatLng {
 
 const getDistance = async (from: LatLng, to: LatLng): Promise<number> => {
   const response = await axios.get(
-    `${osrmHost}/route/v1/driving/${from.lng},${from.lat};${to.lng},${to.lat}?overview=false&generate_hints=false`
+    `${process.env.OSRM}/route/v1/driving/${from.lng},${from.lat};${to.lng},${to.lat}?overview=false&generate_hints=false`
   )
   return response.data.routes[0].distance
 }
