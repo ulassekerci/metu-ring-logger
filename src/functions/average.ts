6007e2c97dad60c6aa8e348d0f0e09c71067ed01
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2025-01-02 12:52:19 +0300

    Updated average trip calculation logic

--- src/functions/average.ts
+++ src/functions/average.ts
@@ -1,23 +1,24 @@
 import { DateTime } from 'luxon'
 import { FormattedTrip } from '../interfaces'
+import outliers from 'outliers'
 
 export const findAverageTrip = (trips: FormattedTrip[], departure: string) => {
   const departureTrips = trips
     .filter((trip) => trip.departure === departure)
     .map((trip) => {
       const departureTime = DateTime.fromFormat(trip.departure, 'HH.mm')
-      const tripEndPoint = trip.points.filter(
-        (point) =>
-          point.address === 'ODTU A1 Kapisi' ||
-          point.address === 'ODTU A2 Kapisi' ||
-          point.address === 'Garajlar' ||
-          point.address === 'BOTE-MYO'
-      )
-      const tripEnd = DateTime.fromJSDate(new Date(tripEndPoint.reverse()[0].timestamp))
-      return { ...trip, durationFromDeparture: tripEnd.diff(departureTime, 'seconds').seconds }
+      const redPoints = trip.points.filter((point) => point.color === '#ff0000')
+      const firstRedPoint = redPoints.reverse()[0]
+      const tripEnd = DateTime.fromJSDate(new Date(firstRedPoint.timestamp))
+      return { ...trip, durationToSouth: tripEnd.diff(departureTime, 'seconds').seconds }
     })
-
-  const sortedTrips = departureTrips.sort((a, b) => a.durationFromDeparture - b.durationFromDeparture)
-  const medianIndex = Math.floor(sortedTrips.length / 2)
-  return sortedTrips[medianIndex]
+  const filteredTrips = departureTrips.filter(outliers('durationToSouth'))
+  const totalDuration = filteredTrips.reduce((acc, trip) => acc + trip.durationToSouth, 0)
+  const averageDuration = totalDuration / filteredTrips.length
+  const tripClosestToAverage = departureTrips.reduce((acc, trip) => {
+    const tripDiff = Math.abs(trip.durationToSouth - averageDuration)
+    const accDiff = Math.abs(acc.durationToSouth - averageDuration)
+    return tripDiff < accDiff ? trip : acc
+  })
+  return tripClosestToAverage
 }
