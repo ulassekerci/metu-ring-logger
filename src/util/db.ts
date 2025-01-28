226e2300b3b7cdbb1505a2b81dd91031953f369c
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2025-01-28 16:19:26 +0300

    Set database connection timezone to Europe/Istanbul

--- src/util/db.ts
+++ src/util/db.ts
@@ -2,6 +2,10 @@ import postgres from 'postgres'
 import 'dotenv/config'
 
 if (!process.env.PGHOST) throw new Error('PGHOST not set')
-const sql = postgres(process.env.PGHOST)
+const sql = postgres(process.env.PGHOST, {
+  connection: {
+    TimeZone: 'Europe/Istanbul',
+  },
+})
 
 export default sql
