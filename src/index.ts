2cbdea823b66990f735956374470ec7df7d7af6d
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2025-08-31 17:56:39 +0300

    project setup

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import type { Request, Response } from 'express'
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello')
+})
+
+app.listen(port, () => {
+  console.log(`Running at port ${port}`)
+})
