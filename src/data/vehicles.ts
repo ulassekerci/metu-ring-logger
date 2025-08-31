74a829b6e2fec57059503d96b2cb16579c59d387
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2025-08-31 23:32:39 +0300

    add hardcoded ring service details

---
+++ src/data/vehicles.ts
@@ -0,0 +1,74 @@
+export const vehicles = [
+  {
+    plate: '06 GE 5351',
+    doors: 2,
+    brand: 'Mercedes-Benz',
+    model: 'Intouro',
+  },
+  {
+    plate: '06 GE 5352',
+    doors: 2,
+    brand: 'Mercedes-Benz',
+    model: 'Intouro',
+  },
+  {
+    plate: '06 GE 5354',
+    doors: 2,
+    brand: 'Mercedes-Benz',
+    model: 'Intouro',
+  },
+  {
+    plate: '06 GE 5355',
+    doors: 2,
+    brand: 'Mercedes-Benz',
+    model: 'Intouro',
+  },
+  {
+    plate: '06 BR 2698',
+    doors: 3,
+    brand: 'Mercedes-Benz',
+    model: 'Conecto',
+  },
+  {
+    plate: '06 DN 6789',
+    doors: 3,
+    brand: 'Otokar',
+    model: 'Kent',
+  },
+  {
+    plate: '06 FZ 9974',
+    doors: 2,
+    brand: 'Otokar',
+    model: 'Kent U',
+  },
+  {
+    plate: '06 FZ 6305',
+    doors: 2,
+    brand: 'Otokar',
+    model: 'Kent U',
+  },
+  {
+    plate: '06 DV 5326',
+    doors: 3,
+    brand: 'Otokar',
+    model: 'Kent',
+  },
+  {
+    plate: '06 DF 8063',
+    doors: 3,
+    brand: 'Otokar',
+    model: 'Kent',
+  },
+  {
+    plate: '06 DF 8064',
+    doors: 3,
+    brand: 'Otokar',
+    model: 'Kent',
+  },
+  {
+    plate: '06 FK 8301',
+    doors: 2,
+    brand: 'Otokar',
+    model: 'Doruk',
+  },
+]
