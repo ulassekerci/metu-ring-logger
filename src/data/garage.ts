c50a40f2b4d1e28ebc399fa59cfb18546e960d42
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2025-09-24 21:10:40 +0300

    fix garage polygon

--- src/data/garage.ts
+++ src/data/garage.ts
@@ -5,4 +5,4 @@ const garageNorth = [32.770458, 39.90715] // north
 const garageEast = [32.77249, 39.905048] // east
 const garageWest = [32.768286, 39.906085] // west
 
-export const garagePolygon = turf.polygon([[garageSouth, garageNorth, garageEast, garageWest]])
+export const garagePolygon = turf.polygon([[garageSouth, garageNorth, garageEast, garageWest, garageSouth]])
