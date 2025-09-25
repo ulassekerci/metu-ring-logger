170e736303a1c38f7ec456b75ccb04f9a60417d9
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2025-09-25 16:10:56 +0300

    add schedule api

--- src/routes/schedule.ts
+++ src/routes/schedule.ts
@@ -1,9 +1,18 @@
 import { Request, Response, Router } from 'express'
+import { ringLines } from '../data/lines'
 
 const app = Router()
 
 app.get('/', async (req: Request, res: Response) => {
-  return res.json()
+  return res.json(
+    ringLines.flatMap((line) => {
+      return line.departures.map((departure) => ({
+        colors: line.colors,
+        time: departure,
+        weekend: line.weekend,
+      }))
+    })
+  )
 })
 
 export default app
