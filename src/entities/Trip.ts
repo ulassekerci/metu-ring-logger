0f5e45e2c021e876830fdbaeb7ce6080282b861d
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2026-02-12 00:19:13 +0300

    implemented RingTripError class

--- src/entities/Trip.ts
+++ src/entities/Trip.ts
@@ -1,46 +1,77 @@
 import { ringLines } from '../data/lines'
-import { RingRow } from '../interfaces/ring'
 import { RingLine } from './Line'
 import * as turf from '@turf/turf'
 import { RingPoint } from './Point'
 import { ServiceTime } from './ServiceTime'
+import { generateID } from '../utils/id'
+import { Duration } from 'luxon'
+import { Vehicle } from './Vehicle'
 
 export class RingTrip {
   id: string
   line: RingLine
   points: RingPoint[]
-  vehiclePlate: string
-  departureTime: string
+  vehicle: Vehicle
+  departureTime: ServiceTime | null
   isPartial: boolean = false
 
-  constructor(id: string, rows: RingRow[]) {
+  constructor(id: string, points: RingPoint[]) {
+    if (points.length === 0) throw new RingTripError('no_rows')
+
+    const ringLine = ringLines.find((line) => line.colors.includes(points[0].color.toUpperCase()))
+    if (!ringLine) throw new RingTripError('line_not_found')
+
+    const isSameVehicle = points.some((point) => point.plate !== points[0].plate)
+    if (isSameVehicle) throw new RingTripError('vehicle_mismatch')
+
+    // TODO: check for points that are too far away from polyline
+
     this.id = id
-    this.vehiclePlate = rows[0].plate
-    this.points = rows.map((row) => RingPoint.fromDb(row))
-    const ringLine = ringLines.find((line) => line.colors.includes(rows[0].color.toUpperCase()))
-    if (!ringLine) throw new Error(`${rows[0].color} colored line not found`)
     this.line = ringLine
+    this.vehicle = new Vehicle(points[0].plate)
+    this.points = points.sort((a, b) => b.serviceTime.seconds - a.serviceTime.seconds)
 
-    // if first point is far from departure stop consider a partial trip
-    const firstPoint = this.points.at(-1)
-    // TODO: handle this better
-    if (!firstPoint) throw new Error('Trip has no points')
-
+    // if first point is far from departure count as a partial trip and display
+    // estimated departure with a warning
+    const firstPoint = this.points.at(-1)!
     const departurePoint = this.line.departureStop.turfPoint
-    const firstPointDistance = turf.distance(firstPoint.turfPoint, departurePoint, { units: 'meters' })
-    if (firstPointDistance > 400) this.isPartial = true
+    const firstPointToDeparture = turf.distance(firstPoint.turfPoint, departurePoint, { units: 'meters' })
 
-    if (this.isPartial) {
-      // if departure is unknown estimate departure time from its position
-      // TODO: finding closest stop isn't reliable - use polyline matching
-      // also look at ! thing
-      this.departureTime = this.line.estimateDeparture(firstPoint)
+    if (firstPointToDeparture > 500) {
+      this.isPartial = true
+      const estimatedDeparture = this.line.estimateDeparture(this.points)
+      this.departureTime = estimatedDeparture
     } else {
-      this.departureTime = this.line.getClosestDeparture(firstPoint.serviceTime)
+      const closestDeparture = this.line.getClosestDeparture(this.firstPoint.serviceTime)
+      this.departureTime = closestDeparture
     }
   }
 
-  getClosestPointToNow = () => {
+  static new(point: RingPoint) {
+    const tripID = generateID()
+    return new RingTrip(tripID, [point])
+  }
+
+  static fromLast(trip: RingTrip, newPoint: RingPoint) {
+    const newPoints = [newPoint, ...trip.points]
+    return new RingTrip(trip.id, newPoints)
+  }
+
+  get firstPoint() {
+    return this.points.at(-1)!
+  }
+
+  get lastPoint() {
+    return this.points[0]
+  }
+
+  get duration() {
+    const first = this.firstPoint.serviceTime.seconds
+    const last = this.lastPoint.serviceTime.seconds
+    return Duration.fromObject({ seconds: last - first })
+  }
+
+  get closestPointToNow() {
     return this.points.reduce((prev, curr) => {
       const now = ServiceTime.now()
       const prevDiff = prev.serviceTime.diff(now)
@@ -49,11 +80,29 @@ export class RingTrip {
     })
   }
 
-  // getClosestPointToTime = (time: ServiceTime) => {
-  //   return this.points.reduce((prev, curr) => {
-  //     const prevDiff = prev.serviceTime.diff(time)
-  //     const currDiff = curr.serviceTime.diff(time)
-  //     return currDiff < prevDiff ? curr : prev
-  //   })
-  // }
+  toJSON() {
+    return {
+      id: this.id,
+      line: this.line.name,
+      color: this.lastPoint.color,
+      vehicle: this.vehicle.plate,
+      departureTime: this.departureTime,
+      closestPointToNow: this.closestPointToNow,
+      isPartial: this.isPartial,
+    }
+  }
+}
+
+export class RingTripError extends Error {
+  problem: 'no_rows' | 'line_not_found' | 'vehicle_mismatch'
+  constructor(problem: typeof RingTripError.prototype.problem) {
+    const messageMap: Record<typeof problem, string> = {
+      no_rows: 'No rows provided to create Trip',
+      line_not_found: 'Line not found for the given color',
+      vehicle_mismatch: 'All rows must have the same plate',
+    }
+    const message = messageMap[problem]
+    super(message)
+    this.problem = problem
+  }
 }
