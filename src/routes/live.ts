08c49bfbab8df197808ae75f71778e33b640ec3d
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2026-02-12 00:21:41 +0300

    Refactor routes to use new trip data classes

--- src/routes/live.ts
+++ src/routes/live.ts
@@ -1,10 +1,10 @@
 import { Request, Response, Router } from 'express'
-import { lastPoll } from '../poller'
+import { liveData } from '../poller/store'
 
 const app = Router()
 
 app.get('/', async (req: Request, res: Response) => {
-  return res.json(lastPoll)
+  return res.json(liveData.trips)
 })
 
 export default app
